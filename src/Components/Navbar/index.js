5ade69e5686a71466662742d920d04bdbcdb2917
RanjithKatkam <ranjithkatkam68@gmail.com>
2024-07-29 09:16:34 +0530

    Final code for project submission

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,7 +4,9 @@ export default function Navbar(props) {
     const location = useLocation();
     return (
         <div className="navbar">
-            <img src="https://res.cloudinary.com/dwgg5pyqk/image/upload/v1722181350/transaction_zf1liz.png" alt="logo" />
+            <Link className="logo" to="/">
+                <img src="https://res.cloudinary.com/dwgg5pyqk/image/upload/v1722181350/transaction_zf1liz.png" alt="logo" />
+            </Link>
             <Link className="link" to="/add-transaction">
                 <button disabled={location.pathname === "/add-transaction" ? true : false}>Add Transaction</button>
             </Link>
